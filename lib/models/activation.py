1c9e7fb38887e875b7bf46a02268cf05c7e269da
Puiching-Memory <1138663075@qq.com>
2025-05-24 05:54:48 +0000

    feat(.gitignore): 更新忽略文件列表并添加子模块
    - 在 .gitignore 中添加 profiler_log/ 目录
    - 新增 .gitmodules 文件，添加 HolisticTraceAnalysis 子模块
    - 更新 requirements.txt，添加新依赖项
    - 移除 model.py 中的冗余代码
    - 在 activation.py 中添加 SwiGLUFFN 激活函数

--- lib/models/activation.py
+++ lib/models/activation.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 class AGLU(nn.Module):
     """Unified activation function module from https://github.com/kostas1515/AGLU"""
@@ -17,3 +16,28 @@ class AGLU(nn.Module):
         """Compute the forward pass of the Unified activation function."""
         lam = torch.clamp(self.lambd, min=0.0001)
         return torch.exp((1 / lam) * self.act((self.kappa * x) - torch.log(lam)))
+    
+class SwiGLUFFN(nn.Module):
+    def __init__(
+        self,
+        dim,
+        hidden_dim,
+        multiple_of,
+        ffn_dim_multiplier=None,
+        device=None,
+        dtype=None,
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        hidden_dim = int(2 * hidden_dim / 3)
+        # custom dim factor multiplier
+        if ffn_dim_multiplier is not None:
+            hidden_dim = int(ffn_dim_multiplier * hidden_dim)
+        hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
+        self.w1 = nn.Linear(dim, hidden_dim, bias=False, **factory_kwargs)
+        self.w2 = nn.Linear(hidden_dim, dim, bias=False, **factory_kwargs)
+        self.w3 = nn.Linear(dim, hidden_dim, bias=False, **factory_kwargs)
+
+    def forward(self, x):
+        return self.w2(F.silu(self.w1(x)) * self.w3(x))
